61f658383cc7146ea485940501f1b9c87076e1af
azeemosiddiqui <mahmoodiazeem@gmail.com>
2026-08-10 16:49:59 +0530

    Change S3 backend key for Terraform state

--- project1-vpc-10Aug26/backend.tf
+++ project1-vpc-10Aug26/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
   backend "s3" {
     bucket         = "terraform-state-590703363311"
-    key            = "aws-new/terraform.tfstate"
+    key            = "aws-quick/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
