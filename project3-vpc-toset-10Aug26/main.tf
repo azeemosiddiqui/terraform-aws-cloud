223696bdb330b63c2cce7f930e17095f4ef0862c
Azeem Siddiqui <mahmoodiazeem@gmail.com>
2026-08-10 17:49:43 +0530

    pushing

---
+++ project3-vpc-toset-10Aug26/main.tf
@@ -0,0 +1,18 @@
+/*###
+resource "aws_vpc" "block-vpc" {
+  cidr_block           = "10.0.0.0/16"
+}
+
+resource "aws_vpc" "block-vpc" {
+  for_each = toset(["30.0.0.0/16","30.0.1.0/16"])
+
+  cidr_block           = each.value
+}
+###*/
+
+### main
+resource "aws_vpc" "block-vpc" {
+  for_each = toset(var.vpcs)
+
+  cidr_block           = each.value
+}
