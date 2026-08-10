cc2cf2fae7ad386cdd07896c41a9c12394bdac6c
azeemosiddiqui <mahmoodiazeem@gmail.com>
2026-08-10 18:05:54 +0530

    Update backend.tf

--- project3-vpc-toset-10Aug26/backend.tf
+++ project3-vpc-toset-10Aug26/backend.tf
@@ -2,7 +2,7 @@ terraform {
 
   backend "s3" {
     bucket         = "terraform-state-590703363311"
-    key            = "aws-project3/terraform.tfstate"
+    key            = "aws-new1/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-state-lock"
     encrypt        = true
